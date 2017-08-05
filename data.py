e1a817f0a2d9abe97efa931573e03fce1850b23e
hanphps <hanphps1@gmail.com>
2017-08-05 13:24:06 -0400

    Add files via upload

--- data.py
+++ data.py
@@ -2,13 +2,24 @@ def Data():
     a = [
         {
             'name': 'Nonlinear Chemical Dynamics Movies',
-            'type': 'videos',
+            'type': 'video',
             'link': 'http://www.pojman.com/NLCD-movies/NLCD-movies.html'
         },
         {
             'name': 'Quantum Finance',
             'type': 'article',
             'link': 'http://www.quantumforquants.org/quantum-computing/why-quantum-finance/'
+        },
+        {
+            'name': 'Biologist Explains One Concept in 5 Levels of Difficulty - CRISPR',
+            'type': 'video',
+            'link': 'https://www.youtube.com/watch?v=sweN8d4_MUg'
+        },
+        {
+            'name': 'The Game of Blackjack and Analysis of Counting Cards',
+            'type': 'paper',
+            'link': 'http://www.annaklis.com/uploads/6/4/7/2/6472295/zimran_klis_fuster_rivelli.gametheory.blackjack.pdf'
         }
+
     ]
     return a
