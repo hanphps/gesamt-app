cd64175ded256d64378c84f55299fa1a412adf95
hanphps <hanphps1@gmail.com>
2017-08-05 02:06:51 -0400

    Update web.py

--- web.py
+++ web.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    name = request.values.get('name')
+    return render_template('index.html', name=name)
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    """
     app.run()
+    """
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
