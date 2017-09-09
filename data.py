5d5816189ecaf379543c5dc95e83df2f9166949c
hanphps <hanphps1@gmail.com>
2017-09-08 20:40:10 -0400

    Update data.py

--- data.py
+++ data.py
@@ -14,6 +14,16 @@ def Data():
             'name': 'Gravitational Waves Observed',
             'type': 'article',
             'link': 'http://www.news.gatech.edu/features/gravitational-waves-observed'
+        },
+        {
+            'name': 'Nonlinear Dynamics In Acoustics',
+            'type': 'paper',
+            'link': 'https://www.researchgate.net/publication/265628910_Nonlinear_dynamics_in_acoustics'
+        },
+        {
+            'name': 'Nonlinear Acoustics In Fluids',
+            'type': 'book',
+            'link': 'https://link.springer.com/referenceworkentry/10.1007%2F978-0-387-30425-0_8#page-1'
         }
 
     ]
